bdd4fe1d00cb6f8a2ffd43fd5104464f1a9983ac
mholliday6611 <mholliday6611@gmail.com>
2018-06-30 10:11:10 -0400

    some models

--- models.js
+++ models.js
@@ -1,1 +1,20 @@
 const mongoose = require('mongoose')
+
+const volunteer = new mongoose.Schema({
+	first_name: String,
+	last_name: String,
+	password: String,
+	email: String
+})
+
+const organization = new mongoose.Schema({
+	org_name: String,
+	password: String,
+	email: String,
+	events: []
+})
+
+module.exports = {
+	Volunteer : mongoose.model("volunteer",volunteer),
+	organization : mongoose.model("organization", organization)
+}
